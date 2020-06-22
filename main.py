f5feef576c70a183dfb90ba4d2308be531fb7a2a
HoopDead <polec0@wp.pl>
2020-06-22 20:31:55 +0200

    Database checker,
    Made a class to check if there's data in database to relieve some unnecessary procces

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from init.database_creator import Creator
+from init.database_checker import Checker
 from automation.create_some_accounts import AccountCreatorAutomat
 from sockets.server import ServerClass
 from sockets.client import ClientClass
 
 
 print("[Main.py] Starting Database Creator")
-database_creator = Creator()
-database_creator.create_db()
-database_creator.create_client_table()
+# database_creator = Creator()
+# database_creator.create_db()
+# database_creator.create_client_table()
 
-accounts_creator = AccountCreatorAutomat()
-accounts_creator.create_multiple_accounts()
+database_checker = Checker()
+database_checker.check_if_database_exists()
+database_checker.check_if_accounts_in_table_exists()
+database_checker.check_if_table_exists()
+
+
+# accounts_creator = AccountCreatorAutomat()
+# accounts_creator.create_multiple_accounts()
