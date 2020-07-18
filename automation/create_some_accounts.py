a201359ee42d44a9217cf2af22f72fe6a8d2ed98
HoopDead <polec0@wp.pl>
2020-07-18 16:18:24 +0200

    Updated database with balance column

--- automation/create_some_accounts.py
+++ automation/create_some_accounts.py
@@ -20,7 +20,8 @@ class AccountCreatorAutomat(Creator):
             r = response.json()
             person = {
                 "first_name": r["results"][0]['name']['first'].encode("utf-8"), 
-                "last_name": r["results"][0]['name']['last'].encode("utf-8"), 
+                "last_name": r["results"][0]['name']['last'].encode("utf-8"),
+                "balance": randint(100, 10000),
                 "address": r["results"][0]["location"]["country"] + ", " + r["results"][0]["location"]["city"] + ", " + r["results"][0]["location"]["street"]["name"] + ", " + str(r["results"][0]["location"]["street"]["number"]), 
                 "account_number": randint(11111111111111111111111111, 99999999999999999999999999), 
                 "credit_card_number": randint(1111111111111111, 9999999999999999), 
@@ -31,8 +32,8 @@ class AccountCreatorAutomat(Creator):
             password_encrypter = AuthenticationPasswordEncrypter()
             password_encrypter.set_secret_key()
             password = password_encrypter.password_encode(person["password"].encode("utf-8"))
-            statement = ("INSERT INTO accounts (name, surname, address, account_number, creditcard, cvv, login_number, password) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
-            values = (person["first_name"], person["last_name"], person["address"], person["account_number"], person["credit_card_number"], person["cvv"], person["login_number"], password)
+            statement = ("INSERT INTO accounts (name, surname, balance, address, account_number, creditcard, cvv, login_number, password) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
+            values = (person["first_name"], person["last_name"], person["balance"], person["address"], person["account_number"], person["credit_card_number"], person["cvv"], person["login_number"], password)
             try:
                 cursor.execute(statement, values)
             except pymysql.err.InternalError:
--- init/database_creator.py
+++ init/database_creator.py
@@ -26,7 +26,7 @@ class Creator:
         cursor = connection.cursor()
         cursor.execute("USE clients")
         try:
-            cursor.execute("CREATE TABLE accounts (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), surname VARCHAR(255), address VARCHAR(255), account_number VARCHAR(26), creditcard VARCHAR(16), cvv VARCHAR(3), login_number VARCHAR(8), password VARCHAR(255))")
+            cursor.execute("CREATE TABLE accounts (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), surname VARCHAR(255), balance VARCHAR(16), address VARCHAR(255), account_number VARCHAR(26), creditcard VARCHAR(16), cvv VARCHAR(3), login_number VARCHAR(8), password VARCHAR(255))")
         except Exception:
             print("[Database_Creator.py] Error (CONTINUE): Table already exsits")
             pass
