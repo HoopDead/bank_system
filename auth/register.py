6dc335c5cb8cc2dbfcc66dc497cc666b15da2359
HoopDead <polec0@wp.pl>
2020-08-21 11:55:16 +0200

    Made functions for check, if there's no account number and credit card number in database already#9

--- auth/register.py
+++ auth/register.py
@@ -24,6 +24,28 @@ class UserRegisterClass(CreatorClass, AuthenticationPasswordEncrypterClass):
         login_numbers = [int(item[0]) for item in result]
         return login_numbers
 
+    def fetch_account_numbers(self):
+        connection = self._connection()
+        cursor = connection.cursor()
+        cursor.execute("USE clients")
+        query = "SELECT account_number FROM accounts;"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        account_numbers = [int(item[0]) for item in result]
+        return account_numbers
+
+    def fetch_credit_card_numbers(self):
+        connection = self._connection()
+        cursor = connection.cursor()
+        cursor.execute("USE clients")
+        query = "SELECT creditcard FROM accounts;"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        credit_cards = [int(item[0]) for item in result]
+        return credit_cards
+
+
+
     def get_login_number(self):
         login_numbers = self.fetch_login_numbers()
         login_number = randint(11111111, 99999999)
@@ -31,6 +53,23 @@ class UserRegisterClass(CreatorClass, AuthenticationPasswordEncrypterClass):
             return get_login_number()
         else:
             return login_number
+
+    def get_account_number(self):
+        account_numbers = self.fetch_account_numbers()
+        account_number =  randint(11111111111111111111111111, 99999999999999999999999999)
+        if account_number in account_numbers:
+            return get_account_number()
+        else:
+            return account_number
+
+    def get_credit_card_number(self):
+        credit_card_numbers = self.fetch_credit_card_numbers()
+        credit_card_number = randint(1111111111111111, 9999999999999999)
+        if credit_card_number in credit_card_numbers:
+            return get_credit_card_number()
+        else:
+            return credit_card_number
+
             
     def register(self):
         connection = self._connection()
@@ -41,8 +80,8 @@ class UserRegisterClass(CreatorClass, AuthenticationPasswordEncrypterClass):
         password_bytes = self.password.encode("utf-8")
         password_encode = auth_login.password_encode(password_bytes)
         login_number = self.get_login_number()
-        account_number =  randint(11111111111111111111111111, 99999999999999999999999999)
-        credit_card_number = randint(1111111111111111, 9999999999999999)
+        account_number = self.get_account_number()
+        credit_card_number = self.get_credit_card_number()
         cvv =  randint(100, 999)
         address = self.country + "," + self.city + "," + self.street_name + "," + self.home_number
         balance = 0
@@ -53,4 +92,4 @@ class UserRegisterClass(CreatorClass, AuthenticationPasswordEncrypterClass):
             cursor.execute(statement, values)
             connection.commit()
         except pymysql.err.InternalError:
-            print("Smth went wrong")
+            print("[Register.py] Something went wrong - please check your data and try again.")
